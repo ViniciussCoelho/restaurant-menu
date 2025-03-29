9ff930653754b2a8372c76e403c8a609ad0f217e
Vinicius Coelho <vinicius2001@gmail.com>
2025-03-29 16:53:14 -0300

    feat: add menu item name uniquiness

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,5 +1,5 @@
 class MenuItem < ApplicationRecord
   belongs_to :menu
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
---
+++ db/migrate/20250329195134_add_unique_index_to_menu_items_name.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexToMenuItemsName < ActiveRecord::Migration[7.0]
+  def change
+    add_index :menu_items, :name, unique: true
+  end
+end
