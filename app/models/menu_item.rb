8c137745578bf698f5e2a9ce10e8af21e034b338
Vinicius Coelho <vinicius2001@gmail.com>
2025-03-30 17:22:17 -0300

    fix: model validation problems

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,6 +1,12 @@
 class MenuItem < ApplicationRecord
-  has_many :menu_listings
+  has_many :menu_listings, dependent: :restrict_with_error
   has_many :menus, through: :menu_listings
 
+  before_destroy :ensure_no_menu_listings
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  def ensure_no_menu_listings
+    errors.add(:base, 'Cannot delete menu item with active listings') if menu_listings.exists?
+  end
 end
